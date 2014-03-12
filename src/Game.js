22d5c92ecc3fc1d273082e3e17ccadcdb3842c3e
elmore <elmore_tom@hotmail.com>
2014-03-12 09:41:44 +0000

    removed game logic so can be used for teaching

--- src/Game.js
+++ src/Game.js
@@ -8,60 +8,11 @@ function Game(grid) {
 
 	grid.render(document.body);
 	
-	var countSetNeighbours = function(x, y) {
-	
-		var count = 0;
-	
-		grid.onEachNeighbourCell(x, y, function(nx, ny) {
-		
-			if(grid.isCellSet(nx, ny)) {
-			
-				count++;
-			}
-		});
-		
-		return count;
-	};
-	
-	var isVeryLonely = function(x, y) {
-	
-		return (countSetNeighbours(x, y) < 2);
-	};
-	
-	var isOverCrowded = function(x, y) {
-		
-		return (countSetNeighbours(x, y) > 3);
-	};
-	
-	var isHuddled = function(x, y) {
-	
-		return (countSetNeighbours(x, y) === 3);
-	};
 	
 	return {
 
 		nextIteration : function() {
 		
-			var toUnset = [];
-			var toSet = [];
-		
-			grid.onEachCell(function(x, y) {
-			
-				if(grid.isCellSet(x, y) && 
-				(isVeryLonely(x, y) || isOverCrowded(x, y))) {
-					
-					toUnset.push({ x : x, y : y });
-				}
-				
-				if(!grid.isCellSet(x, y) && isHuddled(x, y)) {
-				
-					toSet.push({ x : x, y : y });
-				}
-				
-			});
-			
-			grid.set(toSet);
-			grid.unset(toUnset);
 		}
 	};
 }
